8096edb2cd3c1488d243116de4a283af1499b3ec
Lucifer Beatus <imadeddiinemenouer@gmail.com>
2026-06-08 20:17:58 +0100

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ import BottomNav from './BottomNav';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-[var(--color-bg-primary)] transition-colors">
       <TopBar />
       <main className="pb-20">
         <Outlet />
