97ee670fef4f320f613b62f8d7ee438c591a612b
Lucifer Beatus <imadeddiinemenouer@gmail.com>
2026-06-08 19:59:40 +0100

    Update ChapterPage.tsx

--- src/pages/ChapterPage.tsx
+++ src/pages/ChapterPage.tsx
@@ -5,5 +5,13 @@ import ChapterReader from '../components/novels/ChapterReader';
 export default function ChapterPage() {
   const { chapterId } = useParams<{ chapterId: string }>();
 
-  return <ChapterReader />;
+  if (!chapterId) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-gray-500">Chapter not found</p>
+      </div>
+    );
+  }
+
+  return <ChapterReader chapterId={chapterId} />;
 }
