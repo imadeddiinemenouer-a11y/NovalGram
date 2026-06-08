a64dd321d95dee1f3c59d21b71cb02fe720686d7
Lucifer Beatus <imadeddiinemenouer@gmail.com>
2026-06-08 22:24:55 +0100

    Update NovelCard.tsx

--- src/components/novels/NovelCard.tsx
+++ src/components/novels/NovelCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Star, Eye, BookOpen, Clock } from 'lucide-react';
+import { Star, Eye, BookOpen } from 'lucide-react';
 import { formatNumber } from '../../utils/helpers';
 import type { Novel } from '../../types';
 
