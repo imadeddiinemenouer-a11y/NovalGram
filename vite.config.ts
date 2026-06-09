8b06829a115a95a57e6077b609f2526472ffd032
Lucifer Beatus <imadeddiinemenouer@gmail.com>
2026-06-09 20:40:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    logOverride: { 'ts-error': 'silent' },
-    tsconfigRaw: {
-      compilerOptions: {
-        jsx: 'react-jsx',
-        skipLibCheck: true,
-        noEmit: true,
-        strict: false,
-        noUnusedLocals: false,
-        noUnusedParameters: false,
-      },
-    },
-  },
   build: {
     target: 'esnext',
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'TS_ERROR') return;
-        warn(warning);
-      },
-    },
   },
 })
